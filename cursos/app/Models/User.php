a605860725e78a7321f95eaf7d27c5e20cfa37b8
MMartinJ <marina.m.jurado86@gmail.com>
2025-09-27 00:16:10 +0200

    seeder faker factory 2

--- cursos/app/Models/User.php
+++ cursos/app/Models/User.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+//mutadores y accesores
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -42,4 +45,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    /**
+     * Mutador y accesor
+     * Crearemos una función name para la columna name de la tabla users
+     */
+     protected function name(): Attribute{
+        return new Attribute(
+            set: function($value){
+                return strtolower($value);
+            }
+        );
+     }   
+
 }
--- cursos/database/seeders/DatabaseSeeder.php
+++ cursos/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(CursoSeeder::class);
+        \App\Models\Curso::factory(50)->create();
+        \App\Models\User::factory(10)->create();
     }
 }
