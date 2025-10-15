d0bbd0d65a7b0a7ec24e0fbbf469843ad6399a09
MMartinJ <marina.m.jurado86@gmail.com>
2025-10-16 01:18:46 +0200

    creacion de un blog, creacion de las tablas y relaciones

---
+++ blog/app/Models/Post.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    //relacion uno a muchos inversa
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    //relacion uno a muchos inversa
+    public function category(): BelongsTo{
+        return $this->belongsTo(Category::class);
+    }
+
+    //relacion de muchos a muchos
+    public function tags(): BelongsToMany{
+        return $this->belongsToMany(Tag::class);
+    }
+
+    //relacion uno a uno polimorfica
+    public function image(){
+        return $this->morphOne(Image::class, 'imageable');
+    }
+}
---
+++ blog/database/migrations/2025_10_15_225448_create_post_tag_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->id();
+
+            
+            //claves foraneas
+            $table->foreignId('post_id')
+              ->constrained() // por defecto referencia a la tabla "users" y la columna "id"
+              ->onUpdate('cascade')
+              ->onDelete('cascade');
+
+            
+            $table->foreignId('tag_id')
+            ->constrained() 
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_tag');
+    }
+};
